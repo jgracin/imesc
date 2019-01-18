02f58cf56caaa459dafa762f18965c3f79efaf33
Josip Gracin <josip.gracin@gmail.com>
2019-01-18 09:06:12 +0100

    fixed license files and declaration of license

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject imesc "0.1.0-SNAPSHOT"
   :description "Escalation system"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache License, Version 2.0"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/spec.alpha "0.2.176"]
                  [org.clojure/core.specs.alpha "0.2.44"]
