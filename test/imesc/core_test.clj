267679a4024bcf0a3b74f7865af9d2e68184980f
Josip Gracin <josip.gracin@gmail.com>
2019-01-25 09:52:16 +0100

    added stest/check testing of more functions

--- test/imesc/core_test.clj
+++ test/imesc/core_test.clj
@@ -1,7 +1,14 @@
 (ns imesc.core-test
   (:require [imesc.core :as sut]
-            [clojure.test :refer :all]))
+            [clojure.test :refer :all]
+            [clojure.spec.test.alpha :as stest]))
+
+(def num-tests 20)
 
 (deftest transformation
-  (testing "must be able to transform request into alarm specification"
-    (is true true)))
+  (testing "must be able to transform request into alarm entry"
+    (let [results (stest/check [`imesc.core/alarm-entry
+                                `imesc.core/valid?
+                                `imesc.core/decide-next-action]
+                               {:clojure.spec.test.check/opts {:num-tests num-tests}})]
+      (is (not-any? #(contains? % :failure) results)))))
