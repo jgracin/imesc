d470903a3763dfefb924e57884214e3555184b0d
Josip Gracin <josip.gracin@gmail.com>
2019-01-25 09:50:03 +0100

    cleanup of experimental dependencies and code

--- src/imesc/debug.clj
+++ src/imesc/debug.clj
@@ -1,28 +1,11 @@
 (ns imesc.debug
-  (:require [clojure.core.async :as async]
-            [imesc.case-repository :as case-repository]
-            [imesc.config :as config]
+  (:require [imesc.config :as config]
             [integrant.core :as ig]
-            [ring.adapter.jetty :as jetty]
-            [ring.util.response :as resp]
             [clojurewerkz.quartzite.scheduler :as scheduler]
             [clojurewerkz.quartzite.jobs :as jobs :refer [defjob]]
             [clojurewerkz.quartzite.triggers :as triggers]
             [clojurewerkz.quartzite.schedule.simple :as simple]
-            [manifold.deferred]
-            [manifold.stream]
             [imesc.config :as config]))
-
-(comment
-  "General logic"
-  (let [repository (:case-repository @config/system)
-        request-source (:request-source @config/system)]
-    (doseq [request (request-stream request-source)]
-      (if (valid? request)
-        (when-not (alarm-repository/save repository (activator/make-alarm request))
-          (logger/error "unable to save"))
-        (logger/error "invalid request"))))
-  )
 
 (comment
   "Quartzite scheduler"
@@ -38,101 +21,6 @@
                                          (simple/with-repeat-count 10)
                                          (simple/with-interval-in-milliseconds 1000)))))
   (scheduler/schedule s job1 trigger)
-  )
-
-(comment
-  "Manifold"
-  (def s (manifold.stream/stream))
-  (manifold.stream/put! s (str (java.util.UUID/randomUUID)))
-  (manifold.stream/take! s)
-  (def s2 (manifold.stream/periodically 3000 (fn [] (println "Tick") (str  (java.util.UUID/randomUUID)))))
-  (def result (manifold.stream/take! s2))
-  (manifold.deferred/realized? result)
-  (println @result)
-  (manifold.stream/close! s2)
-  (def running? (atom true))
-  (reset! running? false)
-  (reset! running? true)
-  (def input-stream (manifold.stream/stream 2))
-  (manifold.stream/map (fn [v] (println v) v) input-stream)
-  (future
-    (loop []
-      (let [msg (manifold.stream/take! input-stream)]
-        (println "taking...")
-        (println @msg)
-        (Thread/sleep 1000)
-        (println "finished"))
-      (when @running?
-        (recur))))
-  (manifold.stream/put! input-stream (str (java.util.UUID/randomUUID)))
-  )
-
-(comment
-  "core.async"
-
-  (def stdout-lock (Object.))
-  (def c1 (async/chan (async/dropping-buffer 2)))
-  (def running? (atom true))
-  (reset! running? false)
-  (reset! running? true)
-  (let [lock (Object.)]
-    (defn output [& args]
-      (locking lock (apply println args))))
-  (async/go-loop []
-    (output "taking...")
-    (output "v:" (async/<! c1))
-    (output "finished")
-    (Thread/sleep 1000)
-    (when @running?
-      (recur)))
-  (def f1 (future
-            (let [start-time (System/currentTimeMillis)]
-              (output "IN F")
-              (async/>!! c1 (java.util.UUID/randomUUID))
-              (output "FINISHED F in" (str (- (System/currentTimeMillis) start-time)) "ms"))))
-  (realized? f1)
-)
-
-
-(comment
-  "integrant configuration testing"
-  (def test-system (-> config/config
-                       (remove-components [:imesc/case-repository])
-                       (merge {:imesc/in-memory-case-repository {:max-size 5}})
-                       ig/prep
-                       ig/init))
-
-  (ig/halt! test-system)
-  (let [s (make-in-memory-case-repository)]
-    (save-case s {:t 1})
-    (save-case s {:t 2})
-    #_(delete-case s {:t 1})
-    s)
-
-  (def c1 (async/chan 10))
-  (def c (atom 0))
-  (def ec (atom true))
-  (async/go-loop [cnt 0]
-    (try
-      (async/>! c1 cnt)
-      (Thread/sleep 1000)
-      (println (async/<! c1))
-      (reset! c cnt)
-      (catch Exception e (println "Exception:" (.getMessage e))))
-    (when @ec (recur (inc cnt))))
-  (async/go
-    (println (async/<! c1)))
-  (async/>!! c1 1000)
-  (reset! ec false)
-
-  (defn index-handler [{:keys [session] :as request}]
-    (let [new-count (inc (:count session 0))
-          new-session (assoc session :count new-count)]
-      (-> (resp/response (str "Homepage: count=" new-count))
-          (assoc :session new-session))))
-
-  (defn case-information-handler [{:keys [route-params]}]
-    (resp/response (str "You're getting case: " (:id route-params))))
   )
 
 (comment
